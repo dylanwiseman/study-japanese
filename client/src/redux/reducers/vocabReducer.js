5807bc0373e661d527f9ea9fe4eeb3155ce3afd5
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-10-19 16:17:20 -0600

    my edit

--- client/src/redux/reducers/vocabReducer.js
+++ client/src/redux/reducers/vocabReducer.js
@@ -1,9 +1,10 @@
-const initialState = {};
+const initialState = { i: 0 };
 
 export default function vocabReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_VOCAB": {
       return {
+        state,
         ...action.value,
       };
     }
