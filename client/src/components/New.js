d075de63961d6ba7891619e42724ebd1c449d0cf
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-10-20 19:27:22 -0600

    my edit

--- client/src/components/New.js
+++ client/src/components/New.js
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 export default function New() {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const [newWord, setNewWord] = useState("");
+  const [newReading, setNewReading] = useState("");
+  const [newDefinition, setNewDefinition] = useState("");
+
+  async function createVocab(newWordObject) {
+    const { data } = await axios.post(
+      "http://localhost:5004/vocab/new",
+      newWordObject
+    );
+    console.log("data returned from create request:", data);
+    return data;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newWordObject = {
+      word: newWord,
+      reading: newReading,
+      definition: newDefinition,
+    };
+    const data = createVocab(newWordObject);
+    dispatch({
+      type: "GET_VOCAB",
+      value: data,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="word">Word: </label>
+        <input
+          type="text"
+          id="word"
+          value={newWord}
+          onChange={(e) => {
+            setNewWord(e.target.value);
+          }}
+        />
+        <label htmlFor="reading">Reading: </label>
+        <input
+          type="text"
+          id="reading"
+          value={newReading}
+          onChange={(e) => {
+            setNewReading(e.target.value);
+          }}
+        />
+        <label htmlFor="definition">Definition: </label>
+        <input
+          type="text"
+          id="definition"
+          value={newDefinition}
+          onChange={(e) => {
+            setNewDefinition(e.target.value);
+          }}
+        />
+        <input type="submit" value="Add New Word" />
+      </form>
+    </div>
+  );
 }
