bc85f23f293aa69fb1601342c3dc153d28af4cc0
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-10-28 13:42:03 -0600

    styling

--- client/src/components/Reading.js
+++ client/src/components/Reading.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Reading(props) {
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <button
-        className="top-actions-buttons"
+        className={
+          toggle ? "top-actions-buttons" : "top-actions-buttons clicked"
+        }
         id="reading-button"
         onClick={() => {
+          setToggle(!toggle);
           console.log("onclick fn");
           props.toggleReading();
         }}
